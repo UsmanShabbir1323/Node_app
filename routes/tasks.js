e0b28afa5839382f80b61a37eca06c95f6face14
UsmanShabbir1323 <usmanshabbir202@gmail.com>
2025-07-09 14:17:36 +0500

    TS-8 feature: implement helm for CD instead of Raw yaml

--- routes/tasks.js
+++ routes/tasks.js
@@ -18,7 +18,7 @@ router.post('/', async (req, res) => {
 });
 
 router.get('/msg', async (req, res) => {
-    res.status(200).send("Hello world from Argo CD app *** :)");
+    res.status(200).send("Hello world from Argo CD app v11 :)");
 });
 
 router.get('/health', async (req, res) => {
