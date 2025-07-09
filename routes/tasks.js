db791de142d0d1e49e9c57a1c95e624829d8ccb4
UsmanShabbir1323 <usmanshabbir202@gmail.com>
2025-07-09 17:00:29 +0500

    TS-9 feature: deploy dev and prod environment separately

--- routes/tasks.js
+++ routes/tasks.js
@@ -18,7 +18,7 @@ router.post('/', async (req, res) => {
 });
 
 router.get('/msg', async (req, res) => {
-    res.status(200).send("Hello world from Argo CD app v11 :)");
+    res.status(200).send("Hello world from Argo CD app v30 :)");
 });
 
 router.get('/health', async (req, res) => {
